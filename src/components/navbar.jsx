d56d857443663726eeaeb0ba1ce241b99ca4cf0f
sneha1519-ag <sneha221agarwal@gmail.com>
2025-03-27 12:45:45 +0530

    change landing page

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -16,11 +16,11 @@ const Navbar = () => {
     const [menuState, setMenuState] = React.useState(false)
     return ( <header>
         <nav data-state={menuState && 'active'} className="fixed z-20 w-full border-b border-dashed bg-white backdrop-blur md:relative dark:bg-zinc-950/50 lg:dark:bg-transparent">
-            <div className="m-auto max-w-5xl px-6">
+            <div className="m-auto container px-6">
                 <div className="flex flex-wrap items-center justify-between gap-6 py-3 lg:gap-0 lg:py-4">
                     <div className="flex w-full justify-between lg:w-auto">
                         <Link href="/" aria-label="home" className="flex items-center space-x-2">
-                         Task Pulse
+                         <Image src="/logo.svg" alt="TaskPulse" width={200} height={50} className="dark:invert" />
                         </Link>
 
                         <button onClick={() => setMenuState(!menuState)} aria-label={menuState == true ? 'Close Menu' : 'Open Menu'} className="relative z-20 -m-2.5 -mr-4 block cursor-pointer p-2.5 lg:hidden">
